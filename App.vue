56f0ab77968481c3640dcf9766ecb90cb86dc36a
马浩 <1920199810@qq.com>
2024-03-14 19:01:00 +0800

    提交路由跳转监听

--- App.vue
+++ App.vue
@@ -1,6 +1,57 @@
 <script>
 	export default {
+    	data(){
+			return {
+			 
+			}
+		},
+    methods:{
+    
+    },
 		onLaunch: function () {
+      const whiteList=['/pages/login/login', '/pages/home/index', '/pages/contract/index']
+      function hasPermission(url) {
+        // 在白名单中或有token，直接跳转
+        const token = uni.getStorageSync('token') || ''
+        // if(url===undefined) return true
+        if (whiteList.indexOf(url) === -1 && !token) {
+          return true
+        }
+        console.log(555)
+        return false
+      }
+      uni.addInterceptor('navigateTo', { //监听跳转
+         invoke(e) {
+              if (hasPermission(e.url)) {
+                uni.reLaunch({
+                  url: '/pages/login/login'
+                })
+                return false
+              }
+              return true
+            },
+				success(e) {
+					
+				}
+			})
+			uni.addInterceptor('redirectTo', { //监听关闭本页面跳转
+				success(e) {
+				}
+			})
+			uni.addInterceptor('switchTab', { //监听tabBar跳转
+          invoke(e) {
+              if (hasPermission(e.url)) {
+                uni.reLaunch({
+                  url: '/pages/login/login'
+                })
+                return false
+              }
+              return true
+            },
+				success(e) {
+				}
+			})
+
 			console.log('App Launch')
 		},
 		onShow: function () {
--- utils/routeIntercept.js
+++ utils/routeIntercept.js
@@ -16,6 +16,7 @@ function hasPermission(url) {
 uni.addInterceptor('navigateTo', {
   // 页面跳转前进行拦截, invoke根据返回值进行判断是否继续执行跳转
   invoke(e) {
+    console.log(e,'----')
     if (hasPermission(e.url)) {
       uni.reLaunch({
         url: '/pages/login/login'
@@ -32,6 +33,7 @@ uni.addInterceptor('navigateTo', {
 uni.addInterceptor('switchTab', {
   // tabbar页面跳转前进行拦截
   invoke(e) {
+    console.log(e,'----')
     if (hasPermission(e.url)) {
       uni.reLaunch({
         url: '/pages/login/login'
@@ -41,6 +43,6 @@ uni.addInterceptor('switchTab', {
     return true
   },
   success(e) {
-    // console.log(e)
+    console.log(e,'------')
   }
 })
